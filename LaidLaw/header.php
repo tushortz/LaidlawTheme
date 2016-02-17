80595a77bf581209ca5470ebb5887c2307d2d895
sc14tsk <sc14tsk@leeds.ac.uk>
2016-02-17 18:38:38 +0000

    skeleton theme

---
+++ LaidLaw/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Laidlaw theme</title>
+	<link rel="stylesheet" href="<?php bloginfo('style.css'); ?>">
+</head>
+
+<body>
+	<div id="wrapper">
+	<div id="header">
+	<h1>HEADER</h1>
+</div>
